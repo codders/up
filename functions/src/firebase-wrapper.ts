bc9cf8ea331866bcadfcb35793e0627f6d6c37fa
Arthur Taylor <codders@octomonkey.org.uk>
2019-03-05 19:42:46 +0100

    Refactor to hide firebase a little

---
+++ functions/src/firebase-wrapper.ts
@@ -0,0 +1,58 @@
+import * as admin from 'firebase-admin';
+
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: "https://up-now-a6da8.firebaseio.com"
+});
+
+// Express middleware that validates Firebase ID Tokens passed in the Authorization HTTP header.
+// The Firebase ID token needs to be passed as a Bearer token in the Authorization HTTP header like this:
+// `Authorization: Bearer <Firebase ID Token>`.
+// when decoded successfully, the ID Token content will be added as `req.user`.
+export const validateFirebaseIdToken = (req: express.Request, res: express.Response, next: () => void) => {
+  console.log('Check if request is authorized with Firebase ID token', req.method);
+  if (req.method == 'OPTIONS') {
+    next();
+    return;
+  }
+
+  if ((!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) &&
+      !(req.cookies && req.cookies.__session)) {
+    console.error('No Firebase ID token was passed as a Bearer token in the Authorization header.',
+        'Make sure you authorize your request by providing the following HTTP header:',
+        'Authorization: Bearer <Firebase ID Token>',
+        'or by passing a "__session" cookie.');
+    res.status(403).send('Unauthorized');
+    return;
+  }
+
+  let idToken;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+    console.log('Found "Authorization" header');
+    // Read the ID Token from the Authorization header.
+    idToken = req.headers.authorization.split('Bearer ')[1];
+  } else if(req.cookies) {
+    console.log('Found "__session" cookie');
+    // Read the ID Token from cookie.
+    idToken = req.cookies.__session;
+  } else {
+    // No cookie
+    res.status(403).send('Unauthorized');
+    return;
+  }
+
+  admin.auth()
+    .verifyIdToken(idToken)
+    .then(decodedIdToken => {
+      console.log('ID Token correctly decoded', decodedIdToken);
+      req.user = decodedIdToken;
+      console.log('Set user property of req');
+      next();
+    })
+    .catch(error => {
+      console.error('Error while verifying Firebase ID token:', error);
+      res.status(403).send('Unauthorized');
+    });
+};
+
+export { firestore } from 'firebase-admin';
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,66 +1,11 @@
 import './express-types';
 import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
-
-admin.initializeApp({
-  credential: admin.credential.applicationDefault(),
-  databaseURL: "https://up-now-a6da8.firebaseio.com"
-});
+import { validateFirebaseIdToken, firestore } from './firebase-wrapper';
 
 const express = require('express')
 const cookieParser = require('cookie-parser')();
 const cors = require('cors')({origin: true});
 const app = express();
-
-// Express middleware that validates Firebase ID Tokens passed in the Authorization HTTP header.
-// The Firebase ID token needs to be passed as a Bearer token in the Authorization HTTP header like this:
-// `Authorization: Bearer <Firebase ID Token>`.
-// when decoded successfully, the ID Token content will be added as `req.user`.
-const validateFirebaseIdToken = (req: express.Request, res: express.Response, next: () => void) => {
-  console.log('Check if request is authorized with Firebase ID token', req.method);
-  if (req.method == 'OPTIONS') {
-    next();
-    return;
-  }
-
-  if ((!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) &&
-      !(req.cookies && req.cookies.__session)) {
-    console.error('No Firebase ID token was passed as a Bearer token in the Authorization header.',
-        'Make sure you authorize your request by providing the following HTTP header:',
-        'Authorization: Bearer <Firebase ID Token>',
-        'or by passing a "__session" cookie.');
-    res.status(403).send('Unauthorized');
-    return;
-  }
-
-  let idToken;
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
-    console.log('Found "Authorization" header');
-    // Read the ID Token from the Authorization header.
-    idToken = req.headers.authorization.split('Bearer ')[1];
-  } else if(req.cookies) {
-    console.log('Found "__session" cookie');
-    // Read the ID Token from cookie.
-    idToken = req.cookies.__session;
-  } else {
-    // No cookie
-    res.status(403).send('Unauthorized');
-    return;
-  }
-
-  admin.auth()
-    .verifyIdToken(idToken)
-    .then(decodedIdToken => {
-      console.log('ID Token correctly decoded', decodedIdToken);
-      req.user = decodedIdToken;
-      console.log('Set user property of req');
-      next();
-    })
-    .catch(error => {
-      console.error('Error while verifying Firebase ID token:', error);
-      res.status(403).send('Unauthorized');
-    });
-};
 
 app.use(validateFirebaseIdToken);
 app.use(cors);
@@ -81,7 +26,7 @@ app.post('/saveRecord', (request: express.Request, response: express.Response) =
   })
   console.log('Saving data: ', recordWithAuth);
 
-  admin.firestore().collection('up').add(recordWithAuth).then(writeResult => {
+  firestore().collection('up').add(recordWithAuth).then(writeResult => {
     console.log('Got write result', writeResult);
     response.status(201).send({
       success: true,
