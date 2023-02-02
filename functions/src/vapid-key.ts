333dd0499bcd5fc0af42f1dc3752ba14040e8e8f
Arthur Taylor <arthur@codders.de>
2023-02-02 19:17:05 +0100

    Remove vapid key from sourcecode

--- functions/src/vapid-key.ts
+++
@@ -1,7 +0,0 @@
-import { Key } from './vapid-key.d'
-
-export const vapidKey: Key = {
-  pub:
-    'BMhULqAR5z--YRwUr9oVsc1zSDL42utdvC919sjN94uFDwOLqO5-CMtD1EkxCdrYCdzWgR8EdRfnUU33oTNPALM',
-  secret: '0X3qIKTaoZVquos5-tU1Dw-24nbt4HKMYgOVIC7Mq3I'
-}
