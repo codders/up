450b4dc28b1407b2f352ee73a65b97856bddd83c
Arthur Taylor <arthur@codders.de>
2023-02-02 19:19:06 +0100

    Update vapid key

--- model/vapid-key.js
+++ model/vapid-key.js
@@ -1,4 +1,4 @@
 export const vapidKey = {
   pub:
-    'BMhULqAR5z--YRwUr9oVsc1zSDL42utdvC919sjN94uFDwOLqO5-CMtD1EkxCdrYCdzWgR8EdRfnUU33oTNPALM',
+    'BLQkBvZj9rYZswcC2TWNn_3A296pI-qoShVTohIwE8Nq8H_lJIehIDTUjCaoGeVoNfopM6KFyiNKd-7YLNnnpuc'
 }
