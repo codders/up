c427126cd2791a9726f7673e2876cbb6aa476af7
Arthur Taylor <arthur@codders.de>
2023-02-10 11:24:18 +0100

    Fix timezone

--- components/WhatsUp.vue
+++ components/WhatsUp.vue
@@ -66,6 +66,7 @@ export default {
     getTime() {
       const dtFormat = new Intl.DateTimeFormat('en-GB', {
         timeStyle: 'short',
+        timeZone: 'Europe/Berlin'
       })
       try {
         return dtFormat.format(this.timestamp * 1000).replace(':', 'h')
